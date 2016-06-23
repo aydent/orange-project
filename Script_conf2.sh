720757e553fc4a80e78deee58010742633d02177
aydent <aydent@ayt-edge.com>
2016-06-23 13:31:11 +0200

    Update Script_conf2.sh

--- Script_conf2.sh
+++ Script_conf2.sh
@@ -4,7 +4,9 @@ read -p "Quel est votre login d'administration ? :" login #Login d'administrati$
 read -p "Quelle est l'adresse de la machine distante ? : " adresse #adresse de $
 read -p "Quel est le nom du service ? : " service
 
-scp -r ${login}@${adresse}:/etc/${service} /etc/${adresse}
-
+sudo scp -r ${login}@${adresse}:/etc/${service} /etc/${adresse}
+sudo mkdir /tmp/ssl
+sudo scp -r ${login}@${adresse}:/etc/ssl /tmp/ssl
+sudo cp /tmp/ssl/certs/* /etc/ssl/certs/
+sudo rm -r /tmp/ssl
 sudo /etc/init.d/${service} restart
-
